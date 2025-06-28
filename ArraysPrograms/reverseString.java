dc2b308521ae7c9cdfa5e46cc22868636549a640
Parthrajsinh1 <parthrajsinhgol5464@gmail.com>
2025-06-28 17:55:31 +0530

    Update reverseString.java

--- ArraysPrograms/reverseString.java
+++ ArraysPrograms/reverseString.java
@@ -1,7 +1,4 @@
-
 import java.util.Scanner;
-
-
 public class revString{
 	public static void main(String[] args) {
 
@@ -11,16 +8,16 @@ public class revString{
 
 			System.out.println("Your String is : "+s);
 			//Method 1
-			String r = "";
-			for(int i= s.length()-1 ; i>=0 ; i--)
+			String r = ""; // take a empty string 
+			for(int i= s.length()-1 ; i>=0 ; i--) // start for loop in reverse order
 			{
-				r = r + s.charAt(i);
+				r = r + s.charAt(i); // store the original string characters into the reverse string 
 			}
 
 			System.out.println("Your Reverse String is : "+r); 
 			
 			//Method 2
-			String r1 = new StringBuilder(s).reverse().toString();
+			String r1 = new StringBuilder(s).reverse().toString(); //use build in function 
 			System.out.println("Another String is : "+r1);
 	}
 
