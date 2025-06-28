4b6e226acc78a340a74787f900f0a43faa4b8c94
Parthrajsinh1 <parthrajsinhgol5464@gmail.com>
2025-06-28 16:10:55 +0530

    Create arrayList.java
    this program is about how to use ArrayList.

---
+++ ArraysPrograms/arrayList.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+
+public class arrayList{
+	public static void main(String[] args) {
+
+ArrayList arr = new ArrayList(); //ArrayList is more flexible. 
+
+arr.add(111);
+arr.add(222);
+arr.add(333);
+
+System.out.println(arr);
+
+arr.add("mango");
+arr.add("lemon");
+
+System.out.println(arr);
+	}
+
+
+}
